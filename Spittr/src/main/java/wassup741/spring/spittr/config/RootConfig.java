7e5364dd8457386daad6c7d6bc438f5b7bf9de0f
wassup741 <wassup741@gmail.com>
2015-08-16 15:13:48 +0300

    Add "home" page controller

---
+++ Spittr/src/main/java/wassup741/spring/spittr/config/RootConfig.java
@@ -0,0 +1,14 @@
+package wassup741.spring.spittr.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@ComponentScan(basePackages = { "wassup741.spring.spitter" }, excludeFilters = {
+		@Filter(type = FilterType.ANNOTATION, value = EnableWebMvc.class) })
+public class RootConfig {
+
+}
---
+++ Spittr/src/test/java/wassup741/spring/spittr/web/TestTest.java
@@ -0,0 +1,14 @@
+package wassup741.spring.spittr.web;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestTest {
+
+	@Test
+	public void test() {
+		assertEquals(1, 1);
+	}
+
+}
